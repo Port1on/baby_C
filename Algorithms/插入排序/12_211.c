477cfe42ff2e92436e9fd3e0801a86138630a51d
Port1on <67941290+Port1on@users.noreply.github.com>
2020-08-10 17:00:45 +0800

    Create 12_211.c
    题目：运用插入排序法将数组A={5, 2, 4, 6, 1, 3}按从小到大的顺序排列出来。
    
    分析：我认为插入排序法是将从元素A[j]开始与A[1..j-1]（认为A[1..j-1]已排好序）进行比较，插入适当的啥地方后把A[1..j-1]中，并把其他的数字相应的向后移一格，我认为难点就在比较后其他数字移动的过程。

---
+++ Algorithms/插入排序/12_211.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j, A[6] = {5, 2, 4, 6, 1, 3};
+    int key;
+    
+    for(j = 1; j < 6; j++)
+    {
+        key = A[j];
+        i = j-1;
+        while(i>=0 && A[i]>key)
+        {
+            A[i+1] = A[i];
+            i = i-1;
+        }
+        A[i+1] = key;
+    }
+    
+    for(i = 0; i < 6; i++)
+        printf("%4d", A[i]);
+    
+    putchar('\n');
+}
